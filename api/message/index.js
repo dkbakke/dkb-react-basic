4a879ec952f0f00af978de424b5df93aa687d3cb
Dave <dbakkepublic@gmail.com>
2023-06-21 12:48:02 -0700

    API function  index.js extraneous '=' removed

--- api/message/index.js
+++ api/message/index.js
@@ -1,5 +1,10 @@
 module.exports = async function (context, req) {
-    context.res.json = ({
+   context.res.json({
         text : "Hello from your API!"
     });
+   //context.res = {
+   // body: { text : "Hello from your API!" }
+    //};
+//context.done();
+
 };
